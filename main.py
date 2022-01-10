e8474633317645a897055e0f6f0fbbd4372e934d
Lukas <77849373+SushiLeader@users.noreply.github.com>
2022-01-10 20:45:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 import json
 
 
-
+gameid = 6284583030
 def guid_scraper():
     guids = []
     for i in range(3):
         page = random.randint(3000, 3500) + i
-        data = requests.get(f'https://www.roblox.com/games/getgameinstancesjson?placeId=6284583030&startIndex={page}', cookies=json.load(open(r'config.json'))).json()
+        data = requests.get(f'https://www.roblox.com/games/getgameinstancesjson?placeId={gameid}&startIndex={page}', cookies=json.load(open(r'config.json'))).json()
         for server in data['Collection']:
             guids.append(server['Guid'])
     return guids
